10cd467275f3e51dd378f07efbd5b937bfc0ce1e
Leandro Assis <leandrou9@gmail.com>
2022-12-21 10:59:05 -0300

    Tuplas em TypeScript (Compass.Uol)

--- app.ts
+++ app.ts
@@ -2,11 +2,21 @@
     nome: string;
     idade: number;
 } = { */
-const pessoa = {
+const pessoa: {
+    nome: string;
+    idade: number;
+    hobbies: string[];
+    cargo: [number, string];
+} = {
     nome: 'Alejandro',
     idade: 26,
-    hobbies: ['Esporters', 'Culinária']
+    hobbies: ['Esporters', 'Culinária'],
+    cargo: [7, 'Agrimensor']
 };
+
+/* pessoa.cargo.push('Oceanógrafo');
+pessoa.cargo[1] = 10;
+pessoa.cargo = [0, 'oi', 'aaaa']; */
 
 let atividadesFavoritas: string[]; //any[] para qualquer tipo de dado
 atividadesFavoritas = ['Esportes'];
@@ -17,3 +27,5 @@ console.log(pessoa.idade);
 for (const hobby of pessoa.hobbies) {
     console.log(hobby.toUpperCase());
 }
+
+console.log(pessoa.cargo);
