9c2af8e0c7f95efa4b71f235d04d2e00ac76681f
Leandro Assis <leandrou9@gmail.com>
2022-12-21 14:15:53 -0300

    Unknown e Never em TypeScript (Compass.Uol)

--- app.ts
+++ app.ts
@@ -1,32 +1,16 @@
-/* function soma(n1: number, n2: number): string //valor do return {
-    return n1.toString() + n2.toString();
-} */
+let entradaUsuario: unknown; //unknown mais restritivo que any (checa o tipo)
+let nomeUsuario: string;
 
-function soma(n1: number, n2: number) {
-    return n1 + n2;
+//sempre optar por unknown no lugar de any
+entradaUsuario = 5;
+entradaUsuario = 'oioioi';
+if (typeof entradaUsuario === 'string') {
+    nomeUsuario = entradaUsuario;
 }
 
-function mostraResultado(numero: number): void {    //sempre usar void, raro usar undefined
-    console.log('resultado:' + numero);
+//never para uma função que nunca retorna algo
+function gerarErro(mensagem: string, codigo: number): never {
+    throw {mensagem: mensagem, codigoErro: codigo};
 }
 
-function somaSegura(n1: number, n2: number, cb: (num: number) => void) {
-    const resultado = n1 + n2;
-    cb(resultado); //cb = call back
-}
-
-mostraResultado(soma(5, 12));
-
-let valoresCombinados: (a: number, b: number) => number;    //definindo propriedades da função que deve ser chamada
-
-valoresCombinados = soma;
-//valoresCombinados = print;
-//valoresCombinados = 5;
-
-console.log(valoresCombinados(7, 7));
-
-//let algumValor: undefined;
-
-somaSegura(10, 20, (resultado) =>{
-    console.log(resultado);     //não é necessário return, pois o void foi declarado no escopo
-});
+gerarErro('Aconteceu um erro', 500);
