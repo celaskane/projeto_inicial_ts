c4dcd3cb314b758a21c0d0b9bf82ca16ad9bdcb1
Leandro Assis <leandrou9@gmail.com>
2022-12-21 12:54:03 -0300

    Funções tipadas e Callbacks em TypeScript (Compass.Uol)

--- app.ts
+++ app.ts
@@ -10,6 +10,11 @@ function mostraResultado(numero: number): void {    //sempre usar void, raro usa
     console.log('resultado:' + numero);
 }
 
+function somaSegura(n1: number, n2: number, cb: (num: number) => void) {
+    const resultado = n1 + n2;
+    cb(resultado); //cb = call back
+}
+
 mostraResultado(soma(5, 12));
 
 let valoresCombinados: (a: number, b: number) => number;    //definindo propriedades da função que deve ser chamada
@@ -21,3 +26,7 @@ valoresCombinados = soma;
 console.log(valoresCombinados(7, 7));
 
 //let algumValor: undefined;
+
+somaSegura(10, 20, (resultado) =>{
+    console.log(resultado);     //não é necessário return, pois o void foi declarado no escopo
+});
