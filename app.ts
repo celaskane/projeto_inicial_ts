d82ec057c68d38ccd47fe6fad968e01009011f00
Leandro Assis <leandrou9@gmail.com>
2022-12-21 10:37:59 -0300

    Testando objetos com TypeScript (Compass.Uol)

--- app.ts
+++ app.ts
@@ -1,15 +1,11 @@
-function soma(n1: number, n2: number, mostraResultado: boolean, frase: string) {
-    /* if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-        throw new Error('Entrada incorreta');
-    } */
-    const resultado = n1 + n2;
-    if (mostraResultado)
-        console.log(frase + resultado);
-    else return n1 + n2;
-}
+/* const pessoa: {
+    nome: string;
+    idade: number;
+} = { */
+const pessoa = {
+    nome: 'Alejandro',
+    idade: 26
+};
 
-const numero1 = 5;
-const numero2 = 2.8;
-const mostraResultado = true;
-const fraseResultado = 'O resultado é: '
-soma(numero1, numero2, mostraResultado, fraseResultado);
+console.log(pessoa.nome);
+console.log(pessoa.idade);
