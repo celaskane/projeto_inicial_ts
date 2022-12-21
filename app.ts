5a98a1f7673516c859bedd43eb4128b1e72afdff
Leandro Assis <leandrou9@gmail.com>
2022-12-21 10:45:10 -0300

    Arrays em TypeScript (Compass.Uol)

--- app.ts
+++ app.ts
@@ -4,8 +4,16 @@
 } = { */
 const pessoa = {
     nome: 'Alejandro',
-    idade: 26
+    idade: 26,
+    hobbies: ['Esporters', 'Culinária']
 };
+
+let atividadesFavoritas: string[]; //any[] para qualquer tipo de dado
+atividadesFavoritas = ['Esportes'];
 
 console.log(pessoa.nome);
 console.log(pessoa.idade);
+
+for (const hobby of pessoa.hobbies) {
+    console.log(hobby.toUpperCase());
+}
